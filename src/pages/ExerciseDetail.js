9cb85e84a36c49afcf77cc6aa77a5e4335b34f05
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 13:02:00 +0100

    fetching exercise with id

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,12 +3,34 @@ import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import Exercisevideos from "../components/Exercisevideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDBUrl = `https://exercisedb.p.rapidapi.com`;
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDBUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
     <Box>
-      <Detail />
-      <ExerciseVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <Exercisevideos />
       <SimilarExercises />
     </Box>
   );
