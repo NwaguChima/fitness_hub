e0702d570a8852fdf7af53f52691776ee006240e
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 14:31:36 +0100

    completed similar section

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -9,13 +9,24 @@ const SimilarExercises = ({ targetMuscleExercises, equiptmentExercises }) => {
       sx={{
         mt: { lg: "100px", xs: "20px" },
       }}
+      p="20px"
     >
-      <Typography variant="h3" mb={5}>
+      <Typography variant="h3" mb={1}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction="row" sx={{ p: "2", position: "relative" }}>
         {targetMuscleExercises.length ? (
           <HorizontalScrollbar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h3" mb={1} mt="50px">
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equiptmentExercises.length ? (
+          <HorizontalScrollbar data={equiptmentExercises} />
         ) : (
           <Loader />
         )}
