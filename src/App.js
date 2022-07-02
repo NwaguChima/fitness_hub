3586e173abd4540aea7081574eba6e8e1b2a501f
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 02:51:44 +0100

    setup the footer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { Box } from "@mui/material";
 
 import "./App.css";
 
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px sx={{}}">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 };
