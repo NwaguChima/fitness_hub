31d3f940e54470bdf63ac6814a7483dfb1e86216
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 02:30:22 +0100

    setup index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const root = ReactDOM.creatRoot(document.getElementById("root"));
+
+root.render(<App />);
