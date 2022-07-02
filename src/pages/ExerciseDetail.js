f2a567d1b417fd229c340851ca639ab5ab97388e
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 13:30:41 +0100

    fetched video contents

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, youtubeOptions, fetchData } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import Exercisevideos from "../components/Exercisevideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -22,6 +23,12 @@ const ExerciseDetail = () => {
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData);
     };
 
     fetchExercisesData();
