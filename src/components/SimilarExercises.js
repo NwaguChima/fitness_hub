a2dcfa91a542c65f5dbd000dba7190b2fa292f53
NwaguChima <nwaguchima48@gmail.com>
2022-07-02 14:16:41 +0100

    setup scroll bar

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SimilarExercises = () => {
-  return <div>SimilarExercises</div>;
+const SimilarExercises = ({ targetMuscleExercises, equiptmentExercises }) => {
+  return (
+    <Box
+      sx={{
+        mt: { lg: "100px", xs: "0" },
+      }}
+    >
+      <Typography>Exercises that target the same muscle group</Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length && (
+          <HorizontalScrollbar data={targetMuscleExercises} />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
